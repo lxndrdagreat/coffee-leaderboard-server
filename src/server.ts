60b73112e90146e3c61b6f39238947e1d0e01b10
Dan Alexander <lxndrdagreat@gmail.com>
2021-03-01 15:11:31 -0500

    chore: cleanup dependencies

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,10 @@
 import * as express from 'express';
-// import * as https from 'https';
-// import * as fs from 'fs';
-import * as cookieParser from 'cookie-parser';
 import * as config from './config/config';
 import { UnthinkExpressGenerator } from '@epandco/unthink-foundation-express';
 import { UnthinkGenerator } from '@epandco/unthink-foundation';
 import resourceDefinitions from './resource-definitions';
 
 const app: express.Application = express();
-app.use(cookieParser());
 
 const expressGen = new UnthinkExpressGenerator(
   app,
@@ -21,20 +17,5 @@ resourceDefinitions.forEach((rd) => unthinkGen.add(rd));
 
 unthinkGen.printRouteTable();
 unthinkGen.generate();
-
-// if (!config.isProduction) {
-//   // Enable HTTPS for local development
-//   https
-//     .createServer(
-//       {
-//         key: fs.readFileSync('./certs/localhost.key'),
-//         cert: fs.readFileSync('./certs/localhost.crt')
-//       },
-//       app
-//     )
-//     .listen(config.expressServerPort);
-// } else {
-//   app.listen(config.expressServerPort);
-// }
 
 app.listen(config.expressServerPort);
